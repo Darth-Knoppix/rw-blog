eb59074846d7a32531ff16399911ffc5aa6b94e7
Seth Corker <seth.corker@gmail.com>
2021-02-13 13:52:18 +0000

    auth

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -7,19 +7,21 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Private } from '@redwoodjs/router'
 
 const Routes = () => {
   return (
     <Router>
       <Route path="/contact" page={ContactPage} name="contact" />
       <Route path="/blog-post/{id}" page={BlogPostPage} name="blogPost" />
-      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-      <Route path="/admin/posts/{id}/edit" page={EditPostPage} name="editPost" />
-      <Route path="/admin/posts/{id}" page={PostPage} name="post" />
-      <Route path="/admin/posts" page={PostsPage} name="posts" />
-      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
+      <Private unauthenticated="home">
+        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+        <Route path="/admin/posts/{id}/edit" page={EditPostPage} name="editPost" />
+        <Route path="/admin/posts/{id}" page={PostPage} name="post" />
+        <Route path="/admin/posts" page={PostsPage} name="posts" />
+        <Route path="/about" page={AboutPage} name="about" />
+      </Private>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
