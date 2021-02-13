120b35e9fdf126f4850c3a35dd55c4b1fd519cf9
Seth Corker <seth.corker@gmail.com>
2021-02-13 13:36:13 +0000

    contact

--- api/src/services/contacts/contacts.js
+++ api/src/services/contacts/contacts.js
@@ -1,5 +1,21 @@
+import { UserInputError } from '@redwoodjs/api'
 import { db } from 'src/lib/db'
 
 export const contacts = () => {
   return db.contact.findMany()
+}
+
+const validateInput = (input) => {
+  if (input.email && !input.email.match(/[^@]+@[^.]+\..+/)) {
+    throw new UserInputError("Can't create new contact", {
+      messages: {
+        email: ['is not formatted like an email address'],
+      },
+    })
+  }
+}
+
+export const createContact = ({ input }) => {
+  validateInput(input)
+  return db.contact.create({ data: input })
 }
--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -4,29 +4,50 @@ import {
   TextAreaField,
   FieldError,
   Submit,
+  FormError,
 } from '@redwoodjs/forms'
+import { Flash, useFlash, useMutation } from '@redwoodjs/web'
 import BlogLayout from 'src/layouts/BlogLayout'
+import { useForm } from 'react-hook-form'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+  const { addMessage } = useFlash()
+  const [createContact, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      addMessage('Thanks!')
+      formMethods.reset()
+    },
+  })
   const handleSubmit = (data) => {
-    alert(JSON.stringify(data))
+    createContact({ variables: { input: data } })
   }
   return (
     <BlogLayout>
-      <Form onSubmit={handleSubmit}>
+      <Flash timeout={2000} />
+      <Form onSubmit={handleSubmit} error={error} formMethods={formMethods}>
         <label htmlFor="name">Name</label>
         <TextField name="name" validation={{ required: true }} />
         <FieldError name="name" />
 
         <label htmlFor="email">Email</label>
-        <TextField name="email" validation={{ required: true }} />
+        <TextField name="email" type="email" validation={{ required: true }} />
         <FieldError name="name" />
 
         <label htmlFor="message">Message</label>
         <TextAreaField name="message" validation={{ required: true }} />
         <FieldError name="name" />
 
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
+        <FormError error={error} />
       </Form>
     </BlogLayout>
   )
